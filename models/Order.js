62ef93ad187857c1f4720cdd927bc95885e71323
baraasowwan1 <105966196+baraasowwan1@users.noreply.github.com>
2026-06-07 20:02:56 +0300

    Create Order.js

---
+++ models/Order.js
@@ -0,0 +1,33 @@
+quire('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  orderId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  service: {
+    type: String,
+    required: true
+  },
+  serviceId: {
+    type: String,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['completed', 'pending', 'cancelled'],
+    default: 'completed'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Order', orderSchema);
+```
