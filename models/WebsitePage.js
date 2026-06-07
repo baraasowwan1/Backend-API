231bdc103d20074c4d055fcbf60910d7a171be95
baraasowwan1 <105966196+baraasowwan1@users.noreply.github.com>
2026-06-07 20:04:13 +0300

    Create WebsitePage.js

---
+++ models/WebsitePage.js
@@ -0,0 +1,26 @@
+```javascript
+const mongoose = require('mongoose');
+
+const websitePageSchema = new mongoose.Schema({
+  clientId: {
+    type: String,
+    required: true,
+    index: true
+  },
+  pages: {
+    type: Array,
+    default: []
+  },
+  siteName: {
+    type: String,
+    default: 'My Website'
+  },
+  customDomain: String,
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('WebsitePage', websitePageSchema);
+```
